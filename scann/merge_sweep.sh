ecbcc5ad86697f8189ff02528ae9332efa4d7968
YJYJLee <jane001356@gmail.com>
2021-04-07 18:37:06 +0900

    add merge sweep script

---
+++ scann/merge_sweep.sh
@@ -0,0 +1,2 @@
+python3 split_and_run.py --program scann --dataset sift1m --metric squared_l2 --num_split 1 --topk 1000 --batch 128 --sweep
+python3 merge_sweep.py --program scann --dataset sift1m --metric squared_l2 --num_split 1 --topk 1000 --batch 128  --reorder -1
