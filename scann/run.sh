b83cf0aa794a6034dd62f9c0c3bc96e32ce146de
YJYJLee <jane001356@gmail.com>
2021-04-08 05:37:58 +0900

    faiss cpu search config fix

--- scann/run.sh
+++ scann/run.sh
@@ -8,7 +8,7 @@
 # python3 split_and_run.py --program faiss --dataset glove --sweep --topk 1000 --metric squared_l2 --num_split 1 --batch 128
 # python3 split_and_run.py --program faiss --dataset sift1m --sweep --topk 1000 --metric dot_product --num_split 1 --batch 128
 # python3 split_and_run.py --program faiss --dataset glove --sweep --topk 1000 --metric dot_product --num_split 1 --batch 128
-# python3 split_and_run.py --program faiss --dataset deep1b --sweep --topk 1000 --metric squared_l2 --num_split 20 --batch 128
+python3 split_and_run.py --program faiss --dataset deep1b --sweep --topk 1000 --metric squared_l2 --num_split 20 --batch 128
 # python3 split_and_run.py --program faiss --dataset deep1b --sweep --topk 1000 --metric dot_product --num_split 20 --batch 128
 
 # Faiss gpu
@@ -26,10 +26,10 @@
 # python3 split_and_run.py --dataset glove --program scann --sweep --num_split 1 --metric dot_product --topk 1000 --reorder 1000 --batch 128
 # python3 split_and_run.py --dataset sift1b --program scann --sweep --num_split 20 --metric dot_product --topk 1000 --reorder 1000 --batch 128
 
-python3 split_and_run.py --dataset sift1m --program scann --sweep --num_split 1 --metric squared_l2 --topk 1000 --batch 128
-python3 split_and_run.py --dataset glove --program scann --sweep --num_split 1 --metric dot_product --topk 1000 --batch 128
-python3 split_and_run.py --dataset sift1m --program scann --sweep --num_split 1 --metric dot_product --topk 1000 --batch 128
-python3 split_and_run.py --dataset glove --program scann --sweep --num_split 1 --metric squared_l2 --topk 1000 --batch 128
+# python3 split_and_run.py --dataset sift1m --program scann --sweep --num_split 1 --metric squared_l2 --topk 1000 --batch 128
+# python3 split_and_run.py --dataset glove --program scann --sweep --num_split 1 --metric dot_product --topk 1000 --batch 128
+# python3 split_and_run.py --dataset sift1m --program scann --sweep --num_split 1 --metric dot_product --topk 1000 --batch 128
+# python3 split_and_run.py --dataset glove --program scann --sweep --num_split 1 --metric squared_l2 --topk 1000 --batch 128
 # python3 split_and_run.py --dataset sift1b --program scann --sweep --num_split 20 --metric squared_l2 --topk 1000 --batch 128
 # python3 split_and_run.py --dataset sift1b --program scann --sweep --num_split 20 --metric dot_product --topk 1000 --batch 128
 # python3 split_and_run.py --dataset deep1b --program scann --sweep --num_split 20 --metric squared_l2 --topk 1000 --batch 128
